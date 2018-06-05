4e4ddde447899a0c09b5e012b332f7c654aed072
Ariel Altaras <ariel.altaras@gmail.com>
2018-06-04 22:30:50 -0700

    added data to travis file and started parser function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 'use strict';
 
+const http = require('http');
+
 const cowsay = require('cowsay');
+const parser = require('.lib/parser.js');
---
+++ src/lib/parser.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const url = require('url');
+const queryString = require('querystring');
+
+module.exports = (req) => {
+  return new Promise ( (resolve, reject) =>{
+    if(!(req || req.url)) {
+      reject('Request invalid, can not parse.');
+    }
+  } );
+};
