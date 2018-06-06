2945a9e4012a49bc19a7460aca6d2fbe510bf634
Ariel Altaras <ariel.altaras@gmail.com>
2018-06-05 19:00:42 -0700

    writing out parser function

--- src/app.js
+++ src/app.js
@@ -5,24 +5,66 @@ const http = require('http');
 const cowsay = require('cowsay');
 const parser = require('.lib/parser.js');
 
-parser(req)
-  .then( req => {
+const requestHandler = (req, res) => {
 
-    if (req.method === 'GET' && req.url.pathname === '/') {
-      res.setHeader('Content-Type', 'text/html');
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
+  parser(req)
+    .then( req => {
 
-      let message = req.url.query.say;
+      if (req.method === 'GET' && req.url.pathname === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
 
-      res.write(cowsay.say({
-        text : message,
-        e : 'oo',
-        T : 'U'
-      }));
+        res.end();
+        return;
+      }
 
+      else if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
+
+        let message = req.url.query.text;
+
+        res.write(cowsay.say({
+          text : message,
+          e : 'oo',
+          T : 'U',
+        }));
+
+        res.end();
+        return;
+      }
+
+      else if ( req.method === 'POST' && req.url.pathname === '/api/cowsay' ) {
+        res.setHeader('Content-Type', 'text/json');
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
+        res.write( JSON.stringify(req.body) );
+        res.end();
+        return;
+      }
+
+      else {
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 404;
+        res.statusMessage = 'Not Found';
+        res.write('Resource Not Found');
+        res.end();
+      }
+
+    })
+    .catch(err => {
+      res.writeHead(500);
+      res.write(err);
       res.end();
-      return;
-    }
+    });
 
-  });
+};
+
+const app = http.createServer(requestHandler);
+
+module.exports = {
+  start: (port,callback) => app.listen(port,callback),
+  stop: (callback) => app.close(callback),
+};
